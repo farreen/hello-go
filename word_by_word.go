80994b3fcda4cec5e70705dc07c0230e7379e621
Farah Nasreen <farahnasreen2000@gmail.com>
2020-12-07 19:57:46 +0530

    Print words in the reverse order

--- word_by_word.go
+++ word_by_word.go
@@ -10,10 +10,23 @@ func main(){
 		if err!=nil{
 			panic(err)
 		}
+	fmt.Println("Printing word by word in order")
+	for _, word := range words{
+		fmt.Println(word)
+	}
+	// 0        1    2  3     4
+	// nasreen farah is name  my
+	// my       name is farah nasreen
+	//           i       j       
+	fmt.Println("Printing word by word in reverse order")
+	   for i, j := 0, len(words)-1; i < j; i, j = i + 1, j-1 {
+			words[i], words[j] =  words[j], words[i]
+	}
 	for _, word := range words{
 		fmt.Println(word)
 	}
 }
+
 func scanwords(path string) ([]string, error){
 	file, err := os.Open(path)
 		if err!=nil{
@@ -26,5 +39,5 @@ func scanwords(path string) ([]string, error){
 		for scanner.Scan(){
 			words=append(words,scanner.Text())
 		}
-				return words,nil
+       return words,nil
 }
