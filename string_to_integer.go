60aab9c1beb6b5ff658fe864c5443cb098c28512
Farah Nasreen <farahnasreen2000@gmail.com>
2020-12-25 13:11:00 +0530

    convert a string to integer

---
+++ string_to_integer.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	words, err := scanwords("./numbers.txt")
+	if err != nil {
+		panic(err)
+
+	}
+	for _, word := range words {
+		nums, _ := strconv.Atoi(word)
+		result := nums * 2
+		fmt.Println(nums, "*2 =", result)
+	}
+}
+
+func scanwords(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanWords)
+	var words []string
+	for scanner.Scan() {
+		words = append(words, scanner.Text())
+	}
+	return words, nil
+}
